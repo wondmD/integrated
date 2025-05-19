7424cf70da6220b027c9214a44df478168f38353
wondmD <wondmenehdereje@gmail.com>
2025-05-20 01:28:05 +0300

    deleted unneccessary

--- roof_calculator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RoofCalculatorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'roof_calculator'
--- roof_calculator/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.roof_calculate, name='roof_calculate'),
-]
